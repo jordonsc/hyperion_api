ba6cdd98680b21002f97b0a63d612f36bb5f32ca
Nedim Dzendzo <nedim.dzendzo@news.com.au>
2014-10-14 09:21:11 +1100

    fix User class and add instructions on how to create users

--- src/Hyperion/ApiBundle/Entity/User.php
+++ src/Hyperion/ApiBundle/Entity/User.php
@@ -2,10 +2,17 @@
 
 namespace Hyperion\ApiBundle\Entity;
 
-use FOS\UserBundle\Model\User as BaseUser;
+use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ *
+ * 1. create fos_user table based on this class
+ * app/console doctrine:schema:update --force
+ *
+ * 2. create superadmin 'admin' user, will prompt for more info
+ * app/console fos:user:create admin --super-admin
+ *
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
  */
